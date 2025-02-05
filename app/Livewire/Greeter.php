e1d4cdd61fe4af050315ae5232982f529d93a9de
dkaneshi <dkaneshi@gmail.com>
2025-02-04 20:30:32 -1000

    Update Greeter component to add customizable greetings
    Revised the Greeter component to include a dropdown for selecting greetings and updated the name input handling. The greeting message is now dynamically displayed based on user input for both name and selected greeting. Removed unused parameter in the `changeName` method for better alignment with the new workflow.

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -6,11 +6,13 @@ use Livewire\Component;
 
 class Greeter extends Component
 {
-    public $name = 'John';
+    public $name = '';
+    public $greeting = '';
 
-    public function changeName($newName)
+
+
+    public function changeName()
     {
-        $this->name = $newName;
     }
 
     public function render()
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,15 +1,20 @@
 <div>
-    <div>
-        Hello, {{ $name }}!!!
-    </div>
     <form
-        wire:submit="changeName(document.querySelector('#newName').value)"
+        wire:submit="changeName()"
     >
         <div class="mt-2">
+            <select
+                class="p-4 rounded-md  dark:bg-gray-700"
+                wire:model.fill="greeting"
+            >
+                <option value="Hello">Hello</option>
+                <option value="Aloha">Aloha</option>
+                <option value="Hey">Hey</option>
+                <option value="Wassup">Wassup</option>
+            </select>
             <input
-                id="newName"
                 type="text"
-                class="block w-full p-4 rounded-md  bg-gray-700"
+                class="p-4 rounded-md  dark:bg-gray-700"
                 wire:model="name"
             />
         </div>
@@ -22,4 +27,9 @@
             </button>
         </div>
     </form>
+    @if ($name != '')
+        <div class="mt-6">
+            {{ $greeting }}, {{ $name }}!!!
+        </div>
+    @endif
 </div>
